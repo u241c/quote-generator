37534a442320ee2fede9c7252251c6bfd77be557
u241c <jr4701.fsj@gmail.com>
2021-07-12 16:57:28 -0500

    wip: final README.md follow up

---
+++ README.md
@@ -0,0 +1,20 @@
+# Quote Generator
+
+
+
+## Technologies used
+- HTML5
+- CSS3
+- JS
+- [News](https://newsapi.org/)
+- Github Pages (for deployment)
+
+## Screenshots
+
+
+## Getting started
+
+[click] (https://u241c.github.io) here to get started and see the app online! 
+
+## Future Enhancements
+
--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+//show loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show new quote
 function newQuote() {
+    loading();
     // Pick a randon quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // check if Author field is blank and replace it with 'unknown'
@@ -22,11 +36,14 @@ if (quote.text.length > 150){
 } else {
     quoteText.classList.remove('long-quote');
 }
+// Set Quote, Hide Loader    
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // get quotes from API
 async function getQuotes(){
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
@@ -36,5 +53,15 @@ async function getQuotes(){
         // catch Error Here
     }
 }
+
+//Tweet Quote
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listen
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 // on load
 getQuotes();
